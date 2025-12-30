71bbed5285a4edb76bf664997655aa944271aea9
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-30 12:32:52 +0200

    Add functionality to each book's display to change its read status

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ function displayBooks() {
 
         const statusButton = document.createElement("button");
         statusButton.className = "status";
+        statusButton.onclick = () => toggleStatus(book.id);
         if (book.read) {
             statusButton.innerHTML = `
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" /></svg>
@@ -138,7 +139,13 @@ sendBtn.addEventListener("click", (e) =>{
 
 function removeBook(id) {
     myLibrary = myLibrary.filter(book => book.id != id);
-    console.log(`id is ${id}`);
-
     displayBooks();
+}
+
+function toggleStatus(id) {
+    const book = myLibrary.find(book => book.id == id);
+    if (book) {
+        book.read = !book.read;
+        displayBooks();
+    }
 }
